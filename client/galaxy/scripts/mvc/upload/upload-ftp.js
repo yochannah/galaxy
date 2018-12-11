7dd4efae27b8b4f9d42b28afd00c5fcfb90b7bab
Helena Rasche <hxr@hx42.org>
2018-12-11 11:00:28 +0100

    EU: LOCALONLY Fix FTP URL in UI

--- client/galaxy/scripts/mvc/upload/upload-ftp.js
+++ client/galaxy/scripts/mvc/upload/upload-ftp.js
@@ -19,7 +19,7 @@ export default Backbone.View.extend({
             help_text: `This Galaxy server allows you to upload files via FTP. To upload some files, log in to the FTP server at <strong>${
                 options.ftp_upload_site
             }</strong> using your Galaxy credentials.
-            For help visit the <a href="https://galaxyproject.org/ftp-upload/" target="_blank">tutorial</a>.`,
+            For help visit the <a href="https://ftp.usegalaxy.eu" target="_blank">documentation</a>.`,
             collection: null,
             onchange: function() {},
             onadd: function() {},
